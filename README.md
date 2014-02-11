58c5fb93f080d717d265270ffb112f44420d28c8
avail <eric.s.conklin@gmail.com>
2014-02-11 13:15:43 -0500

    readme and stuff

---
+++ README.md
@@ -0,0 +1,27 @@
+How do You make go work?? Good Question.
+
+1 - If go is not in $PATH
+export PATH=$PATH:/usr/local/go/bin
+
+2 - IF GO ROOT IS NOT SET 
+export GOROOT=/usr/local/go/
+
+3 - IF GOPATH is not SET
+export GOPATH=proect_directory/api
+example
+export GOPATH=/home/ef92/code/geocensus/api
+
+Install Stuff we need
+go get github.com/codegangsta/martini
+go get github.com/lib/pq
+
+v = ["12",14,123,235,46,346,346];
+v[0];
+v = {"test":0,"test2":425}
+v.test2
+
+outpyt = getDataFromsite();
+
+geo = output[0]
+
+get.geometry.cooridinates[0]
--- api/server.go
+++ api/server.go
@@ -6,15 +6,16 @@ import (
 	"database/sql"
 	"encoding/json"
 	"github.com/codegangsta/martini"
+	"strings"
 	//"fmt"
 )
 
 type geojson struct {
     Type        string
     Features 	feature
-    Point       Point
-    Line        Line
-    Polygon     Polygon
+    //Point       Point
+    //Line        Line
+    //Polygon     Polygon
 }
 
 type feature struct {
@@ -25,12 +26,12 @@ type feature struct {
 
 type geometry struct {
 	Type        string
-	Coordinates []map[float]float{}
-
+	//Coordinates []map[float]float{}
+}
 func main() {
   m := martini.Classic()
   m.Get("/", func() string {
-    return "Hello world!"
+    return "Hey world!"
   })
   m.Get("/states", func() string {
     
@@ -39,7 +40,6 @@ func main() {
 		return err.Error()
 	}
 
-	//age := 21
 	rows, err := db.Query("SELECT geoid, name FROM tl_2013_us_state ")
 	if err != nil {
 		return err.Error()
@@ -67,7 +67,6 @@ func main() {
 		return err.Error()
 	}
 
-	//age := 21
 	rows, err := db.Query("SELECT geoid, name,ST_AsGeoJSON(the_geom) as geo FROM tl_2013_us_state WHERE geoid = $1",params["geoid"])
 	if err != nil {
 		return err.Error()
@@ -77,19 +76,62 @@ func main() {
 			var name string
 			var geoid string
 			var geo string
-            if err := rows.Scan(&geoid, &name,&geo); err != nil {
+            if err := rows.Scan(&geoid, &name, &geo); err != nil {
                     return err.Error()
             }
+
             state := map[string]string{
-            	"geometry": geo,
-            	"type":"feature",
+            	"geometry": geo ,
+            	"Properties": "{\"geoid\": "+geoid+"}",
+            	"type":"Feature",
             }
+
             states = append(states, state)
+
     }
 	b, err := json.Marshal(states)
-	return string(b)
 
-  //return "Hello " + params["geoid"]
+	/*The Following lines of code are used to parse and remove unnecessary characters that get in the way of 
+	creating proper geojson files*/
+
+	c := strings.Replace(string(b), "\\", "", -1)
+	c = strings.Replace(c, "\":\"", "\":", 1)
+	c = strings.Replace(c, "\",\"g", ",\"g", 1)
+	c = strings.Replace(c, "y\":\"{", "y\":{", 1)
+	c = strings.Replace(c, "\",\"t", ",\"t", 1)
+	d := "{\n  \"type\": \"FeatureCollection\",\n  \"features\": \n"+c+"}"
+	return d
+})
+
+	/*:geoid converts given part of url in a parameter to be potentially used. This param is used as params["x"] 
+	where x is the given parameter*/
+
+m.Get("/states/:geoid/county", func(params martini.Params) string {
+
+	db, err := sql.Open("postgres", "host=lor.availabs.org password=transit user=postgres dbname=geocensus sslmode=require")
+	if err != nil {
+		return err.Error()
+	}
+
+	/*The following code selects the information needed from the above host to be accessed and used here. 
+	$1 is replaced with the given parameter*/
+
+	rows, err := db.Query("SELECT geoid as geo FROM tl_2013_us_state WHERE geoid = $1",params["geoid"])
+	if err != nil {
+		return err.Error()
+	}
+
+	/*The below variables are strings that contain the selected information from the above query*/
+
+	var geoid string
+	for rows.Next() {
+        	if err := rows.Scan(&geoid); err != nil {
+            	return err.Error()
+        	}
+        }
+
+	return "These are the counties for state with geoid: " +geoid+""
+
 })
 
   m.Run()
